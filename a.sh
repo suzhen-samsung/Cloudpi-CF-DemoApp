db7933566740c4eb2d96e89c7fe5f8d863ac4f99
suzhen <suzhen@samsung.com>
2014-07-07 16:48:40 +0800

    a.adjust python/django sqlite;b. add counter.war

---
+++ a.sh
@@ -0,0 +1,2 @@
+python manage.py syncdb --noinput >> ./startup.log 2>&1
+gunicorn_django 
--- pythonDjango4sqlite/mysite/views.py
+++ pythonDjango4sqlite/mysite/views.py
@@ -5,8 +5,12 @@ from books.models import Book
 
 def hello(request):
 	now=datetime.datetime.now()
-	html="<html><body><h3>Hello World!</h3>It is now %s</body></html>" % now
-	return HttpResponse(html)
+	#html="<html><body><h3>Hello World!</h3>It is now %s</body></html>" % now
+	#html="<html><body><h3>Hello World!</h3>It is now %s</body></html>" % now
+        response = HttpResponse()
+        response.write("<p>Hello World</p>")
+	return response
+	#return HttpResponse(html)
 
 def latest_books(request):
 	book_list=Book.objects.order_by('-pub_date')[:10]
